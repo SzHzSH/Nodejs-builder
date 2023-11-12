a58bcb5d68dcbf14f2ceb222dce64d500a4c7a09
Sz Hz <94116026+SzHzSH@users.noreply.github.com>
2023-11-12 12:51:18 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
-wget https://nodejs.org/dist/v21.1.0/node-v21.1.0.tar.xz
-tar -Jxvf node-v21.1.0.tar.xz
-cd node-v21.1.0
-./configure --prefix=$(pwd)/bins
-make -j 8
-make install 
-tar -Jcvf nd21.tar.xz 
+#wget https://nodejs.org/dist/v21.1.0/node-v21.1.0.tar.xz
+#tar -Jxvf node-v21.1.0.tar.xz
+#cd node-v21.1.0
+#./configure --prefix=$(pwd)/bins --dest-cpu arm64 --enable-static --verbose 
+#make -j 8
+#make install 
+#tar -Jcvf nd21.tar.xz 
+echo project failed && exit 1
